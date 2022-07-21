8143acc1beba8ea9f5a37853aeaa176f247143b1
kimdeiz <kimdeiz@naver.com>
2022-07-21 20:22:04 +0900

    오늘은 h2 db 세팅과 test case 작성을 해보았고 외부 라이브러리를 사용하여 log에 쿼리문 파라미터 까지 띄우는 작업을 해보았습니다.

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    // transactional이 test에 붙어있으면 rollback 을 한다.
+    //@Rollback(false)
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        System.out.println(savedId);
+        System.out.println(member.getId());
+
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        // id 값이 같으면 같은 엔티티로 본다.
+        System.out.println("findMember == member: " + (findMember == member));
+
+    }
+}
