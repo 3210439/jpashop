e3b103181fb42fc611897bef63dcc483ea95b698
kimJongWan <kimdeiz@naver.com>
2022-10-15 10:53:30 +0900

    add home screen and layout

--- src/main/java/jpabook/jpashop/controller/HelloController.java
+++ src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -1,17 +1,23 @@
 package jpabook.jpashop.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HelloController {
 
-    @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
     }
+
 }
 
 
